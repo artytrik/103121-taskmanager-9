d1c8e670c609758bfd20dc69c55a5ff852f4617e
artytrik <tema-luch@mail.ru>
2019-08-24 09:33:51 +0300

    Сделал рандомные свойства для дней недели

--- src/data.js
+++ src/data.js
@@ -16,13 +16,13 @@ export const getCard = () => ({
   ][Math.floor(Math.random() * 3)],
   dueDate: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
   repeatingDays: {
-    'Mo': false,
-    'Tu': false,
-    'We': false,
+    'Mo': Boolean(Math.round(Math.random())),
+    'Tu': Boolean(Math.round(Math.random())),
+    'We': Boolean(Math.round(Math.random())),
     'Th': Boolean(Math.round(Math.random())),
-    'Fr': false,
-    'Sa': false,
-    'Su': false,
+    'Fr': Boolean(Math.round(Math.random())),
+    'Sa': Boolean(Math.round(Math.random())),
+    'Su': Boolean(Math.round(Math.random()))
   },
   tags: new Set([
     `homework`,
